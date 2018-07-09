80f3e62d0e6b629bb67b77bf03ea12cd69363d29
Mike <soto77mas@hotmail.com>
2018-07-09 18:35:37 -0500

    Se creo la vista para agregar foto, y se hizo la conexion de back para mandar datos de crear usuario

--- app/crear-usuario/crear-usuario.component.ts
+++ app/crear-usuario/crear-usuario.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DataService } from "../data.service";
+import { Global } from "../interfaces/int.Global";
+import { CrearResp } from "../interfaces/int.CrearResp";
+import { HttpModule, Http, URLSearchParams, Headers, RequestOptions} from '@angular/http';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-crear-usuario',
@@ -7,9 +14,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearUsuarioComponent implements OnInit {
 
-  constructor() { }
 
+  apiRoot: string = "http://lacocs.montielpalacios.com/usuario";
+  global: Global;
+  constructor(private router: Router, private data: DataService, private http: Http) { }
+
+  crearMessage: string= "";
   ngOnInit() {
+  }
+
+  crearUsuario(rol_usuario_id: string, email: string, contrasena: string, nombre: string, apellido: string, /*nss: string,*/ laboratorio_id: string, fechaDeNac: string){
+      
+  	  /*
+  	  if(strlenght = 0)
+  	  {
+
+  	  }
+  	  
+
+  	  const FORMATO_ENTRADA = 'DD-MM-YYYY';
+	  const FORMATO_SALIDA = 'YYYY-MM-DDTHH:mm:ss Z';
+ 	  const fecha = moment('15/06/2018',FORMATO_ENTRADA);
+
+	  console.log(fecha.format(FORMATO_SALIDA))
+	  let FORMATO_
+
+	*/
+
+      this.data.currentGlobal.subscribe(global => this.global = global);
+      let url = `${this.apiRoot}/get/endpoint.php`;
+      let search = new URLSearchParams();
+      search.set('function', 'insert');
+      search.set('token', this.global.token);
+      search.set('rol_usuario_id', "1001");
+      search.set('nombre', nombre);
+      search.set('apellido', apellido);
+      search.set('email', email);
+      //search.set('nss', nss);
+      search.set('fechaDeNac', fechaDeNac);
+      search.set('rol_usuario_id_new', rol_usuario_id);
+      search.set('constrasena', contrasena);
+
+      
+      this.http.get(url, {search}).subscribe(res => this.diplay(res.json()) );
+
+}
+
+diplay(crearResp: CrearResp){
+    if(crearResp.error==0){
+   	  this.router.navigate(['administrador/insertar-foto']);
+    }else{
+      this.crearMessage=crearResp.estatus;
+    }
   }
 
 }
--- app/layouts/admin-layout/admin-layout.routing.ts
+++ app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { CrearUsuarioComponent } from '../../crear-usuario/crear-usuario.component';
+import { InsertarFotoComponent } from '../../insertar-foto/insertar-foto.component';
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
 import { IconsComponent } from '../../icons/icons.component';
@@ -16,6 +17,7 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
     { path: 'crear-usuario',   component: CrearUsuarioComponent },
+    { path: 'insertar-foto',   component: InsertarFotoComponent },
     { path: 'prueba',  		  component: PruebaComponent },
     { path: 'table-list',     component: TableListComponent },
     { path: 'typography',     component: TypographyComponent },
