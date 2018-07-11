ae6996f26d8036849e25ba728cc707214f1beab9
Daniel F <inx.crow@gmail.com>
2018-07-11 18:02:43 -0500

    alertas de herramientas activas

---
+++ app/admin/crear-herramientas/Herramienta.ts
@@ -0,0 +1,11 @@
+export class Herramienta {
+
+  constructor(
+   public 	descripcion: string,
+   public	tipo: string,
+   public	fechaDeComp: string,
+   public	condicion: string,
+   public	laboratorio_id: string
+  
+  ) {  }
+}
