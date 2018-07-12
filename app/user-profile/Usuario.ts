c700f3233cb6b839d7145d59ba99528855b12ee1
Daniel F <inx.crow@gmail.com>
2018-07-12 13:30:51 -0500

    perfil ya recibe datos y esta bindeado

---
+++ app/user-profile/Usuario.ts
@@ -0,0 +1,13 @@
+export class Usuario 
+{
+	constructor(
+	public id_usuario: string,
+	public nombre: string,
+  	public apellido: string,
+  	public email: string,
+  	public fechaDeNac: string,
+  	public foto: string,
+  	public rol_usuario_id: string
+  	) {  }
+
+}
