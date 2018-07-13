1d575120f74d562b3971297ddccf0048c41ccfd1
Daniel F <inx.crow@gmail.com>
2018-07-13 13:44:42 -0500

    dropdown y conexion realizadas

--- app/user-profile/user-profile.component.ts
+++ app/user-profile/user-profile.component.ts
@@ -21,14 +21,22 @@ import {
 })
 export class UserProfileComponent implements OnInit {
 
-    id_usuario: string;
+    puestos= ['Jefe de Brigada', 'Jefe de Laboratorio', 
+              'Administrativo', 'Tecnico de campo']
+    id_usuario: string ;
     nombre: string;
     apellido: string;
     email: string;
     fechaDeNac: string;
     foto: string;
-    rol_usuario_id: string;
+    laboratorio_id: string;
+    laboratorio: string;
+    nss: string;
+    rol: string;
     submitted = false;
+    hidden = false;
+    mis_roles: Array<any>;
+    mis_lab: Array<any>;
     onSubmit() { this.submitted = true; }
 
   loginMessage: string= "";
@@ -38,27 +46,108 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit() {
     this.data.currentGlobal.subscribe(global => this.global = global);
-    let url = `${this.global.apiRoot}/usuario/get/endpoint.php`;
-	let search = new URLSearchParams();
+
+    let url = `${this.global.apiRoot}/rol/get/endpoint.php`;
+  let search = new URLSearchParams();
+  search.set('function', 'getAll');
+    search.set('token', this.global.token);
+    search.set('rol_usuario_id', "1001");
+  this.http.get(url, {search}).subscribe(res => this.llenaRoles(res.json()) );
+
+     url = `${this.global.apiRoot}/laboratorio/get/endpoint.php`;
+    search = new URLSearchParams();
+    search.set('function', 'getAll');
+    search.set('token', this.global.token);
+    search.set('rol_usuario_id', "1001");
+    this.http.get(url, {search}).subscribe(res => this.llenaLaboratorio(res.json()) );
+
+
+    url = `${this.global.apiRoot}/usuario/get/endpoint.php`;
+	 search = new URLSearchParams();
 	search.set('function', 'getIDByToken');
     search.set('token', this.global.token);
     search.set('rol_usuario_id', "1001");
 	this.http.get(url, {search}).subscribe(res => this.llenado(res.json()) );
   }
 
+
+  llenaRoles(resp: any)
+  {
+    this.mis_roles= new Array(resp.length);
+    var j=resp.length-1;
+    for (var _i = 0; _i < resp.length; _i++ )
+    {
+      this.mis_roles[_i]=resp[j];
+      j--;
+
+    }
+  }
+
+  llenaLaboratorio(resp: any)
+  {
+    this.mis_lab= new Array(resp.length);
+    for (var _i = 0; _i < resp.length; _i++ )
+    {
+      this.mis_lab[_i]=resp[_i];
+
+    }
+  }
+
+  mostrar()
+  {
+    this.hidden=true;
+  }
+  ocultar()
+  {
+    this.hidden=false;
+
+
+  }
+
+
+  actualizarUsuario(nombre: string, apellido: string,
+                    laboratorio_id: string, nss:string,
+                    email: string, fechaDeNac: string,
+                    id_usuario: string, rol_usuario_id: string, )
+  {
+    let url = `${this.global.apiRoot}/usuario/post/endpoint.php`;
+    let formData:FormData = new FormData();
+    formData.append('function', 'upDate');
+    formData.append('token', this.global.token);
+    formData.append('rol_usuario_id', '1001');
+
+
+    formData.append('id_usuario', id_usuario);
+    formData.append('nombre', nombre);
+    formData.append('apellido', apellido);
+    formData.append('laboratorio_id', laboratorio_id);
+    formData.append('nss', nss);
+    formData.append('email', email);
+    formData.append('fechaDeNac', fechaDeNac);
+    formData.append('rol_usuario_id_new', rol_usuario_id);
+
+    this.http.post(url, formData).subscribe(res => this.respuestaError(res.json()) );
+
+
+  }
+
+
+  respuestaError(resp: any){
+    if(resp.error!=0)
+    {
+      window.alert(resp.estatus);
+      location.reload();
+    }
+    else
+    {
+      location.reload();
+    }
+  }
+
+
   llenado(respuesta: any)
   {
-    console.log(respuesta);
     this.model=respuesta;
-    /*this.id_usuario= respuesta.id_usuario;
-    this.nombre= respuesta.nombre;
-    this.apellido= respuesta.apellido;
-    this.email= respuesta.email;
-    this.fechaDeNac= respuesta.fechaDeNac;
-    this.foto= respuesta.foto;
-    this.rol_usuario_id= respuesta.rol_usuario_id;*/
-
-
   }
   
   
@@ -68,7 +157,10 @@ export class UserProfileComponent implements OnInit {
                        this.apellido,
                        this.fechaDeNac,
                        this.foto,
-                       this.rol_usuario_id);
+                       this.rol,
+                       this.nss,
+                       this.laboratorio,
+                       this.laboratorio_id);
 
 
 }
