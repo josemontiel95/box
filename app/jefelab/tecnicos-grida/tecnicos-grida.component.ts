cb35f4e51ed57969cd7ab8a361fe169365405e6b
NightmareVisions <dxn.walker23@gmail.com>
2018-08-11 12:10:20 -0500

    punto estable de tecnicos y herramienta falta conexion correcta al back

--- app/jefelab/tecnicos-grida/tecnicos-grida.component.ts
+++ app/jefelab/tecnicos-grida/tecnicos-grida.component.ts
@@ -3,29 +3,25 @@ import { HttpModule, Http, URLSearchParams, Headers, RequestOptions} from '@angu
 import { DataService } from "../../data.service";
 import { Global } from "../../interfaces/int.Global";
 import { Router, ActivatedRoute } from '@angular/router';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-
 
 @Component({
-  selector: 'app-herramienta-grida',
-  templateUrl: './herramienta-grida.component.html',
-  styleUrls: ['./herramienta-grida.component.css']
+  selector: 'app-tecnicos-grida',
+  templateUrl: './tecnicos-grida.component.html',
+  styleUrls: ['./tecnicos-grida.component.css']
 })
-export class HerramientaGridAgregaComponent implements OnInit  {
+export class TecnicosGridAgregaComponent implements OnInit  {
 	title = 'app';
   global: Global;
   private gridApi;
   private gridColumnApi;
   rowSelection;
   columnDefs;
-  id: string;
 
   constructor( private http: Http, private router: Router, private data: DataService, private route: ActivatedRoute){
 	  this.columnDefs = [
-    {headerName: 'ID', field: 'id_herramienta' },    
-    {headerName: 'Tipo', field: 'tipo' },
-    {headerName: 'Placas', field: 'placas' },
-    {headerName: 'Condicion', field: 'condicion'},
+      {headerName: 'ID', field: 'id_cliente'},
+      {headerName: 'Tecnico.', field: 'nombre'+'apellido' },
+
 
       
     ];
@@ -35,35 +31,30 @@ export class HerramientaGridAgregaComponent implements OnInit  {
   rowData: any;
 
   ngOnInit() {
-        this.data.currentGlobal.subscribe(global => this.global = global);
-        this.id= localStorage.getItem("herra");
   }
 
 
   onGridReady(params) {
     this.data.currentGlobal.subscribe(global => this.global = global);
+    console.log("this.global.apiRoot"+this.global.apiRoot);
+    console.log("this.global.token"+this.global.token);
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
-    let url = `${this.global.apiRoot}/herramienta/get/endpoint.php`;
+    let url = `${this.global.apiRoot}/usuario/get/endpoint.php`;
     let search = new URLSearchParams();
-    search.set('function', 'getAllFromTipo');
+    search.set('function', 'getAllAdmin');
     search.set('token', this.global.token);
     search.set('rol_usuario_id', this.global.rol);
-    search.set('herramienta_tipo_id', this.id);
     console.log(search);
     this.http.get(url, {search}).subscribe(res => {
                                             console.log(res.json());
-                                            this.llenaTabla(res.json());
+                                            //this.llenaTabla(res.json());
                                             this.gridApi.sizeColumnsToFit();
                                           });
   }
 
-
-
-
   llenaTabla(repuesta: any){
     console.log(repuesta)
-
     if(repuesta.error==1 || repuesta.error==2 || repuesta.error==3){
       window.alert(repuesta.estatus);
       this.router.navigate(['login']);
@@ -78,11 +69,10 @@ export class HerramientaGridAgregaComponent implements OnInit  {
     var id = "";
 
     selectedRows.forEach(function(selectedRow, index) {
-      id += selectedRow.id_herramienta;
+      id += selectedRow.id_tecnicos;
       
     });
-      localStorage.setItem('herrasel',id );
-
+    localStorage.setItem('tecnisel',id );
   }
 
 
