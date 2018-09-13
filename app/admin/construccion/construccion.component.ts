c362717a69788c030f5e59bd7d926e41e796e3d2
AVCrow <inx.crow@gmail.com>
2018-09-12 23:19:19 -0500

    se creo en construccion

---
+++ app/admin/construccion/construccion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit} from '@angular/core';
+import { HttpModule, Http, URLSearchParams, Headers, RequestOptions} from '@angular/http';
+import { DataService } from "../../data.service";
+import { Global } from "../../interfaces/int.Global";
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'construccion',
+  templateUrl: './construccion.component.html',
+  styleUrls: ['./construccion.component.css','../../loadingArrows.css']
+})
+export class ConstruccionComponent {
+	 
+}
